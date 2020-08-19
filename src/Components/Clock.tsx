be619c4946c2efb2762fcb5d3540f7eb97bbfec4
m9dnts8608 <leah8608@gmail.com>
2020-08-19 23:47:42 +0900

    Clock Component

--- src/Components/Clock.tsx
+++ src/Components/Clock.tsx
@@ -7,29 +7,31 @@ const Clock = () => {
   const [sec, setSec] = useState("");
 
   const getDate = () => {
-    const startDay = Number(new Date("2019-02-01:00:00:00+0900"));
-    let now = Number(new Date());
-    let remained = startDay - now;
+    setInterval(() => {
+      const startDay = Number(new Date("2019-02-01:00:00:00+0900"));
+      let now = Number(new Date());
+      let remained = startDay - now;
 
-    const DAY = 1000 * 60 * 60 * 24;
-    const HOUR = 1000 * 60 * 60;
-    const MINUTE = 1000 * 60;
-    const SECOND = 1000;
+      const DAY = 1000 * 60 * 60 * 24;
+      const HOUR = 1000 * 60 * 60;
+      const MINUTE = 1000 * 60;
+      const SECOND = 1000;
 
-    const days = Math.abs(Math.floor(remained / DAY));
-    const hours = Math.abs(Math.floor((remained % DAY) / HOUR)) - 1;
-    const minutes = Math.abs(Math.floor((remained % HOUR) / MINUTE)) - 1;
-    const seconds = Math.abs(Math.floor((remained % MINUTE) / SECOND)) - 1;
+      const days = Math.abs(Math.floor(remained / DAY));
+      const hours = Math.abs(Math.floor((remained % DAY) / HOUR)) - 1;
+      const minutes = Math.abs(Math.floor((remained % HOUR) / MINUTE)) - 1;
+      const seconds = Math.abs(Math.floor((remained % MINUTE) / SECOND)) - 1;
 
-    const dayStr = `${days < 10 ? `0${days}` : days}`;
-    const hourStr = `${hours < 10 ? `0${hours}` : hours}`;
-    const minuteStr = `${minutes < 10 ? `0${minutes}` : minutes}`;
-    const secondStr = `${seconds < 10 ? `0${seconds}` : seconds}`;
+      const dayStr = `${days < 10 ? `0${days}` : days}`;
+      const hourStr = `${hours < 10 ? `0${hours}` : hours}`;
+      const minuteStr = `${minutes < 10 ? `0${minutes}` : minutes}`;
+      const secondStr = `${seconds < 10 ? `0${seconds}` : seconds}`;
 
-    setDay(dayStr);
-    setHour(hourStr);
-    setMin(minuteStr);
-    setSec(secondStr);
+      setDay(dayStr);
+      setHour(hourStr);
+      setMin(minuteStr);
+      setSec(secondStr);
+    }, 1000);
   };
 
   useEffect(getDate, [sec]);
